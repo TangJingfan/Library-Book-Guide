3915d33676e51f7118afa628cbc4ccbce63e8a36
tang_jingfan <tangjingfan@gmail.com>
2024-12-29 16:34:17 +0800

    feat: update move.ino laser_to_arduino node

--- workspace/src/car_moving/move/move.ino
+++ workspace/src/car_moving/move/move.ino
@@ -1,31 +1,42 @@
 #include "src/MecanumDriver.h"
 
+// define pin for mecanum car
 MecanumDriver mecanum(9, 8, 12, 13, 11, 10, 46, 21);
 
+//define an array to record speed for 4 wheels
+int speed[4] = {0, 0, 0, 0};
+
+// define a string to receive command from serial
+String command;
+
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Starting setup...");
+  // init mecanum car
   mecanum.begin();
-  Serial.println("Mecanum initialized");
+
+  // start serial; set baudrate at 115200
+  Serial.begin(115200);
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    String command = Serial.readString();  // 读取完整输入字符串
-    command.trim();  // 去掉多余的空格和换行符
-    Serial.print("Received command: ");
-    Serial.println(command);
+  while (Serial.available() > 0) {
 
-    if (command == "w") {
-      mecanum.setDutyCycle(100, 100, 100, 100);
-      Serial.println("Motors running forward");
-    } else {
-      mecanum.setDutyCycle(0, 0, 0, 0);
-      Serial.println("Motors stopped");
-    }
-  } else {
-    mecanum.setDutyCycle(0, 0, 0, 0);
+    // read command from serial
+    char temp = char(Serial.read());
+    command += temp;
   }
-  delay(100);
-}
 
+  if (command.length() > 0) {
+    int begin = command.indexOf('<');
+    int end = command.indexOf('>') + 1;
+    if (begin != -1 && end != -1 && begin < end) {
+
+      // assign value to speed array
+      sscanf(message.substring(begin, end).c_str(), "<%d,%d,%d,%d>", &speed[0],
+             &speed[1], &speed[2], &speed[3]);
+    }
+  }
+
+  // make mecanum car run
+  mecanum.setDutyCycle(speed[0], speed[1], speed[2], speed[3]);
+  delay(20);
+}
--- workspace/src/laser_to_arduino/src/laser_to_arduino.cpp
+++ workspace/src/laser_to_arduino/src/laser_to_arduino.cpp
@@ -3,40 +3,50 @@
 #include <sensor_msgs/LaserScan.h>
 #include <serial/serial.h>
 #include <sstream>
+#include <vector>
 
 // serial object
 serial::Serial ser;
 
 // deal with LaserScan data
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr &msg) {
-  // get min distance
-  float min_distance =
-      *std::min_element(msg->ranges.begin(), msg->ranges.end());
-
-  // send info to arduino
-  std::stringstream ss;
-  ss << "w"
-     << "\n";
-  // ss << "distance:" << min_distance << "\n";
-  std::string command = ss.str();
+  // read from laser and set obstacle
+  std::vector<std::pair<float, float>> obstacles;
+  for (size_t i = 0; i < msg->ranges.size(); i++) {
+    float distance = msg->ranges[i];
+    float angle = msg->angle_min + i * msg->angle_increment;
+    // select useful data and set location in point cloud
+    if (distance >= msg->range_min && distance <= msg->range_max) {
+      float x = distance * cos(angle);
+      float y = distance * sin(angle);
+      obstacles.emplace_back(x, y);
+    }
+  }
 
   try {
-    ser.write(command);
+    // send info to arduino
+    ser.write("<100,100,100,100>");
+    // flush output buffer
     ser.flushOutput();
-    ROS_INFO("Sent to Arduino: %s", command.c_str());
-    ros::Duration(0.1).sleep(); // Sleep for 100 milliseconds
+    std::string received_msgs = ser.read();
+    // ROS_INFO("Sent to Arduino: %s", received_msgs.c_str());
+    std::cout << received_msgs << std::endl;
+    // ros::Duration(0.1).sleep(); // Sleep for 100 milliseconds
   } catch (serial::IOException &e) {
     ROS_ERROR("Unable to send data to Arduino.");
   }
 }
 
 int main(int argc, char **argv) {
+  // init node
   ros::init(argc, argv, "laser_to_arduino");
+
+  // init a node handle
   ros::NodeHandle nh;
 
-  // init serial
   try {
-    ser.setPort("/dev/arduino"); // modify it to real serial
+    // init serial
+    ser.setPort("/dev/arduino");
     ser.setBaudrate(115200);
     serial::Timeout to = serial::Timeout::simpleTimeout(1000);
     ser.setTimeout(to);
@@ -52,10 +62,10 @@ int main(int argc, char **argv) {
     return -1;
   }
 
-  // subscribe
-  ros::Subscriber sub = nh.subscribe("/scan", 10, laserCallback);
+  // subscribe topic "/scan"
+  ros::Subscriber sub = nh.subscribe("/scan", 7, laserCallback);
 
-  // keep running
+  // keep the node running
   ros::spin();
 
   ser.close();
