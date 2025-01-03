91ffff49144bcbfe0c6aa3ee4b5e1754da24dc30
tang_jingfan <tangjingfan@gmail.com>
2025-01-03 16:10:05 +0800

    feat: update contorl logic

--- workspace/src/controlled_move/controlled_move.ino
+++ workspace/src/controlled_move/controlled_move.ino
@@ -2,41 +2,35 @@
 
 MecanumDriver mecanum(9, 8, 12, 13, 11, 10, 46, 21);
 
-char command = 'x'; // defalut command
-unsigned long last_command_time = 0; // last time receive command
-const unsigned long command_timeout = 100; // timeout unit:ms
+String command;
+String lastCommand = "x"; // 初始化为停止状态
 
-void setup() {
+void setup() { 
     mecanum.begin();
     Serial.begin(115200);
 }
 
 void loop() {
-
-    if (Serial.available() > 0) {
+    while (Serial.available() > 0) {
         char temp = char(Serial.read());
         if (temp != '\n' && temp != '\r') { 
-            command = temp; // update command
-            last_command_time = millis(); // record time when command received
+            command = temp; // 只保留最后一次有效指令
         }
     }
 
-    // execute
-    if (command == 'w') {
-        mecanum.setDutyCycle(100, 100, 100, 100); // forward
-    } else if (command == 'a') {
-        mecanum.setDutyCycle(-100, 100, -100, 100); // turn left
-    } else if (command == 's') {
-        mecanum.setDutyCycle(-100, -100, -100, -100); // backward
-    } else if (command == 'd') {
-        mecanum.setDutyCycle(100, -100, -100, 100); // turn right
-    } else if (command == 'x') {
-        mecanum.setDutyCycle(0, 0, 0, 0); // stop
-    }
+    if (command != lastCommand) { // 如果命令有变化，则执行
+        if (command == "w") {
+            mecanum.setDutyCycle(100, 100, 100, 100); // forward
+        } else if (command == "a") {
+            mecanum.setDutyCycle(-100, 100, -100, 100); // turn left
+        } else if (command == "s") {
+            mecanum.setDutyCycle(-100, -100, -100, -100); // backward
+        } else if (command == "d") {
+            mecanum.setDutyCycle(100, -100, -100, 100); // turn right
+        } else if (command == "x") {
+            mecanum.setDutyCycle(0, 0, 0, 0); // stop
+        }
 
-    // timeout check
-    if (millis() - last_command_time > command_timeout) {
-        command = 'x'; // stop
-        mecanum.setDutyCycle(0, 0, 0, 0);
+        lastCommand = command; // 更新上一次命令
     }
 }
