b74cf0e62a8a6acd72a1c9d2a60545bce7b28ae7
tang_jingfan <tangjingfan@gmail.com>
2025-01-20 16:11:20 +0800

    fix: small naming bug in position recorder

--- workspace/src/position_recorder/src/position_recorder.cpp
+++ workspace/src/position_recorder/src/position_recorder.cpp
@@ -41,7 +41,7 @@ int main(int argc, char **argv) {
            << ".csv";
 
   std::ofstream file(filename.str());
-  file << "index,millisecond,x,y,z,yaw" << std::endl;
+  file << "second,millisecond,x,y,z,yaw" << std::endl;
 
   ros::Rate rate(10.0);
 
