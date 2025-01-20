58ee4011abf1d782c0db35e58aa46caf17597df8
tang_jingfan <tangjingfan@gmail.com>
2025-01-20 11:57:43 +0800

    fix: directory name for saving mapg

--- cartographer_workspace/map_save.sh
+++ cartographer_workspace/map_save.sh
@@ -3,4 +3,4 @@
 source install_isolated/setup.bash
 rosservice call /finish_trajectory 0
 rosservice call /write_state "{filename: '/home/nano/Library-Book-Guide/workspace/src/map/map_info/map.pbstream'}"
-rosrun cartographer_ros cartographer_pbstream_to_ros_map -map_filestem=/home/nano/Autonomous-Ball-Pick-up-Car/workspace/src/map/map_info/map -pbstream_filename=/home/nano/Library-Book-Guide/workspace/src/map/map_info/map.pbstream -resolution=0.05
+rosrun cartographer_ros cartographer_pbstream_to_ros_map -map_filestem=/home/nano/Library-Book-Guide/workspace/src/map/map_info/map -pbstream_filename=/home/nano/Library-Book-Guide/workspace/src/map/map_info/map.pbstream -resolution=0.05
