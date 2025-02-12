778a9edf93cbc490eded4776d6991c113db0728f
TangJingfan <tangjingfan@gmail.com>
2025-02-12 12:16:09 +0900

    feat: update

--- workspace/src/controlled_move/controlled_move.ino
+++ workspace/src/controlled_move/controlled_move.ino
@@ -5,32 +5,32 @@ MecanumDriver mecanum(9, 8, 12, 13, 11, 10, 46, 21);
 String command;
 String lastCommand = "x"; // init chassis; setting as 'stop'
 
-void setup() { 
-    mecanum.begin();
-    Serial.begin(115200);
+void setup() {
+  mecanum.begin();
+  Serial.begin(115200);
 }
 
 void loop() {
-    while (Serial.available() > 0) {
-        char temp = char(Serial.read());
-        if (temp != '\n' && temp != '\r') { 
-            command = temp; // keep latest useful command
-        }
+  while (Serial.available() > 0) {
+    char temp = char(Serial.read());
+    if (temp != '\n' && temp != '\r') {
+      command = temp; // keep latest useful command
+    }
+  }
+
+  if (command != lastCommand) { // execute last command if new command is entered
+    if (command == "w") {
+      mecanum.setDutyCycle(50, 50, 50, 50); // forward
+    } else if (command == "a") {
+      mecanum.setDutyCycle(-50, 50, -50, 50); // turn left
+    } else if (command == "s") {
+      mecanum.setDutyCycle(-50, -50, -50, -50); // backward
+    } else if (command == "d") {
+      mecanum.setDutyCycle(50, -50, 50, -50); // turn right
+    } else if (command == "x") {
+      mecanum.setDutyCycle(0, 0, 0, 0); // stop
     }
 
-    if (command != lastCommand) { // execute last command if new command is entered
-        if (command == "w") {
-            mecanum.setDutyCycle(50, 50, 50, 50); // forward
-        } else if (command == "a") {
-            mecanum.setDutyCycle(-50, 50, -50, 50); // turn left
-        } else if (command == "s") {
-            mecanum.setDutyCycle(-50, -50, -50, -50); // backward
-        } else if (command == "d") {
-            mecanum.setDutyCycle(50, -50, 50, -50); // turn right
-        } else if (command == "x") {
-            mecanum.setDutyCycle(0, 0, 0, 0); // stop
-        }
-
-        lastCommand = command; // update command
-    }
+    lastCommand = command; // update command
+  }
 }
