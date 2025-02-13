e10737435ade77f452958eb3e048e6eb04c0c57c
TangJingfan <tangjingfan@gmail.com>
2025-02-13 11:22:44 +0900

    test

--- cartographer_workspace/map_save.sh
+++ cartographer_workspace/map_save.sh
@@ -2,5 +2,5 @@
 
 source install_isolated/setup.bash
 rosservice call /finish_trajectory 0
-rosservice call /write_state "{filename: '/home/nano/Library-Book-Guide/workspace/src/map/map_info/map.pbstream'}"
-rosrun cartographer_ros cartographer_pbstream_to_ros_map -map_filestem=/home/nano/Library-Book-Guide/workspace/src/map/map_info/map -pbstream_filename=/home/nano/Library-Book-Guide/workspace/src/map/map_info/map.pbstream -resolution=0.05
+rosservice call /write_state "{filename: '/home/nano/Library-Book-Guide/workspace/src/map/map_info/B300.pbstream'}"
+rosrun cartographer_ros cartographer_pbstream_to_ros_map -map_filestem=/home/nano/Library-Book-Guide/workspace/src/map/map_info/B300 -pbstream_filename=/home/nano/Library-Book-Guide/workspace/src/map/map_info/B300.pbstream -resolution=0.05
